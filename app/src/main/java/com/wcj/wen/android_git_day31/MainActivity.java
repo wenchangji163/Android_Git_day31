d8ed6f75095ac289228247f1ad313c20b84a7520
wenchangji163 <270844233@qq.com>
2016-03-31 15:12:02 +0800

    第一提交

---
+++ app/src/main/java/com/wcj/wen/android_git_day31/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wcj.wen.android_git_day31;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
